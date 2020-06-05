02bf843a0afe0a3703b4890571be8c7f9a6c6398
aniruddhamonker <aniruddh.amonker@live.com>
2020-06-04 19:53:38 -0700

    retry attempts for connection errors

--- src/contrail_introspect_scrapper/base.py
+++ src/contrail_introspect_scrapper/base.py
@@ -22,23 +22,27 @@ class IntrospectBaseClass():
         self.num_threads = num_threads
         self.__output_files = defaultdict(list)
     
-    @staticmethod
-    def get_request(url):
+    @classmethod
+    def get_request(cls, url, retrcnt=5):
         try:
             response = requests.get(url, timeout=TIMEOUT)
             response.raise_for_status
         except requests.exceptions.ReadTimeout:
             print('Read Timeout Occurred for URL: {}'.format(url))
-        except requests.exceptions.ConnectionError as errc:
-            print ("Error Connecting:",errc)
+        except requests.exceptions.ConnectionError:
+            print ("Error Connecting for url: {}".format(url))
+            print("Retrying {}: retry attempt {}".format(url, retrcnt))
+            if retrcnt >=1:
+                cls.get_request(url, retrcnt-1)
+            else:
+                 print("All retry attempts failed for url: {} , moving on..".format(url))
         except requests.exceptions.RequestException as err:
             print ("OOps: Something Else",err)
-        except Exception as err:
-            print('{} Error encountered : {}\nResponse Code: {}'\
-                   .format(type(err), err, response.status_code))
-        return response.text if isinstance(response, requests.models.Response)\
-             else None
-
+        #except Exception as err:
+         #   print('{} Error encountered : {}\n'\
+           #        .format(type(err), err))
+        return response.text 
+        
     @classmethod
     def parse_response(cls, url, attrs=None):
         text_response = cls.get_request(url)
@@ -50,11 +54,6 @@ class IntrospectBaseClass():
                 yield element
         return __iter__()
 
-    '''
-    def get_index_page_nodes(self):
-        attrs = {'href': re.compile(r'xml')}
-        yield from self.parse_response(self.root_url, attrs=attrs)
-    '''
     def _get_index_page_nodes_url(self):
         all_index_page_node_urls = []
         for node in self.all_nodes:
@@ -63,6 +62,7 @@ class IntrospectBaseClass():
                 self.parse_response(url, {'href': re.compile(r'xml')} )]
             all_index_page_node_urls.extend([url+'/'+index_page_node.get('href')\
                                 for index_page_node in index_page_nodes])
+        #import pdb; pdb.set_trace()
         yield from all_index_page_node_urls            
 
     def _fetch_introspect(self, queue):
@@ -72,27 +72,28 @@ class IntrospectBaseClass():
             index_page_node_url = queue.get()
             filename = "/tmp/{}-{}".format(index_page_node_url.split('/')[-2], index_page_node_url.split('/')[-1])
             threading.current_thread().setName(filename)
-            print("Thread: {} started".format(threading.current_thread().getName()))
+            #print("Thread: {} started".format(threading.current_thread().getName()))
             try:
                 with open(filename, 'w') as op_file:
                     for introspect in self.parse_response(index_page_node_url, \
                         attrs=sandesh_attrs):
-                        try:
-                            op_file.write(START_MARKER(introspect.name)+"\n"+introspect.name+"\n"+END_MARKER(introspect.name)+"\n")
-                            introspect_url = re.sub(r'(http.*/).*$', r'\1', index_page_node_url)+'Snh_'+introspect.name
-                            introspect_response = self.parse_response(introspect_url)
-                            op_file.write(introspect_response.prettify())
-                            op_file.write(END_OF_TEXT)
-                            if introspect_response.findAll(attrs={"link":"SandeshTraceRequest"}):
-                                for sandesh_trace_buf in introspect_response.findAll(attrs={"link":"SandeshTraceRequest"}):
-                                    op_file.write(START_MARKER(sandesh_trace_buf.text)+"\n"+sandesh_trace_buf.text+"\n"+END_MARKER(sandesh_trace_buf)+"\n")
-                                    op_file.write(self.parse_response(re.sub(r'(http.*/).*$', r'\1', index_page_node_url)+'/'+'Snh_SandeshTraceRequest?x='+sandesh_trace_buf.text).prettify())
-                                    op_file.write(END_OF_TEXT)                            
-                            op_file.flush()
-                        except Exception as write_exp:
-                            print("Exception {} for thread {}\nUnable to write output to file {}\n".format(write_exp, threading.current_thread().getName(), filename))
+                        op_file.write(START_MARKER(introspect.name)+"\n"+introspect.name+"\n"+END_MARKER(introspect.name)+"\n")
+                        introspect_url = re.sub(r'(http.*/).*$', r'\1', index_page_node_url)+'Snh_'+introspect.name
+                        introspect_response = self.parse_response(introspect_url)
+                        op_file.write(introspect_response.prettify())
+                        op_file.write(END_OF_TEXT)
+                        if introspect_response.findAll(attrs={"link":"SandeshTraceRequest"}):
+                            for sandesh_trace_buf in introspect_response.findAll(attrs={"link":"SandeshTraceRequest"}):
+                                op_file.write(START_MARKER(sandesh_trace_buf.text)+"\n"+sandesh_trace_buf.text+"\n"+END_MARKER(sandesh_trace_buf)+"\n")
+                                sandesh_trace_url = re.sub(r'(http.*/).*$', r'\1', index_page_node_url)+'Snh_SandeshTraceRequest?x='+ "{}".format(sandesh_trace_buf.text)
+                                op_file.write(self.parse_response(sandesh_trace_url).prettify())
+                                op_file.write(END_OF_TEXT)                            
+                        op_file.flush()
+            except UnboundLocalError:
+                pass
             except Exception as exp:
-                print("Exception {} occurred for {}\nUnable to create output file: {}\n".format(exp, threading.current_thread().getName(), filename))
+                #import pdb; pdb.set_trace()
+                print("Exception {} occurred for {}\nUnable to create output file: {}\n".format(type(exp), threading.current_thread().getName(), filename))
             self.__output_files[index_page_node_url.split('/')[-2]].append(filename)
             queue.task_done()
         return
@@ -109,6 +110,7 @@ class IntrospectBaseClass():
                 threads.append(introspect_thread)    
             except threading.ThreadError as err:
                 print("Failed to create thread {}\n{}\n{}".format(threading.current_thread.__name__, type(err), err))
+        print("Current active thread count is {}\n".format(threading.active_count()))
         for introspect_thread in threads:
             introspect_thread.join()
     
@@ -118,7 +120,7 @@ class IntrospectBaseClass():
         for node in self.all_nodes:
             module_ip = node['url'].split('/')[-1]
             module_name = node['module']
-            tar_filename = '{}/{}-{}-{}.tar.gz'.format(dir, module_name, module_ip, time.strftime('%Y-%m-%d-%H-%M'))
+            tar_filename = '{}/{}-{}-{}.tar.gz'.format(dir, module_name, re.sub(r'(.*):\d+', r'\1', module_ip), time.strftime('%Y-%m-%d-%H-%M'))
             with tarfile.open(tar_filename, mode="w:gz") as tar:
                 try:
                     for file in self.__output_files[module_ip]:
@@ -127,9 +129,9 @@ class IntrospectBaseClass():
                     print("Failed to create a tar file archive for {} due to error:\n{}".format(module_name, terr))
                 except Exception as tarexp:
                     print("Exception of type {} occurred when adding file {} to tar archive\nArchive failed for module {}\n{}".format(type(tarexp), file, module_name, tarexp))
-        tar.close()
+            tar.close()
+            print("Introspect collection completed successfully for node {} and module {}\n".format(module_ip, module_name))
         self.delete_tmp_files(self.__output_files)
-        print("archive complete for node {} and module {}\n".format(module_ip, module_name))
         return
 
     @staticmethod
