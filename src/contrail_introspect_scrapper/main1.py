4385915a671ac3b40196344155074a7ee92bd448
aniruddhamonker <aniruddh.amonker@live.com>
2020-06-25 19:45:53 -0700

    print version

--- src/contrail_introspect_scrapper/main1.py
+++ src/contrail_introspect_scrapper/main1.py
@@ -1,7 +1,14 @@
 from config_parser import ConfigParser
 from base import IntrospectBaseClass
+import sys
+
+version = '0.1.0'
 
 all_nodes = ConfigParser()
-introspect = IntrospectBaseClass(all_nodes(), 50, debug=True)
+# import pdb; pdb.set_trace()
+if all_nodes.version:
+    print("CONTRAIL-INTROSPECT-SCRAPE\nVERSION: {}".format(version))
+    sys.exit()
+introspect = IntrospectBaseClass(all_nodes(), 50, debug=all_nodes.debug)
 introspect.fetch_all_introspects()
 introspect.archive_all_files()
