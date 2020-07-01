1114ed26f2775ab715dacf465aa29bbe71fa1e66
aniruddhamonker <aniruddh.amonker@live.com>
2020-06-30 19:12:22 -0700

    removed module args from base.py

--- src/contrail_introspect_scrapper/config_parser.py
+++ src/contrail_introspect_scrapper/config_parser.py
@@ -2,40 +2,22 @@ import sys
 import argparse
 import yaml
 import re
+from metadata import module_args as margs
 from collections import namedtuple
 
 class ConfigParser():
     def __init__(self):
-        module = namedtuple("mod", "name, option, help, port")    
-        self.args = {
-            "vrouter": module("--vrouter", "-vr","contrail-vrouter", "8085"),
-            "cconfig": module("--cconfig", "-cfg", "contrail-config", "8084"),
-            "ccontrol": module("--ccontrol", "-cnt", "contrail-control", "8083"),
-            "canalytics": module("--canalytics", "-can", "contrail-analytics-api", "8090"),
-            "alarm-gen": module("--alarmgen", "-alg", "contrail-analytics-alargen", "5995"),
-            "discovery": module("--discovery", "-dis", "contrail-discovery", "5997"),
-            "schema": module("--schema", "-sch", "contrail-schema", "8087"),
-            "svc-monitor": module("--svc-mon", "svc", "contrail-svc-monitor", "8088"),
-            "collector": module("--collector", "-col", "contrail-analytics-collector", "8089"),
-            "device-manager": module("--device-manager", "-dm", "contrail-device-manager", "8096"),
-            "contrail-dns": module("--contrail-dns", "-dns", "contrail-dns", "8092"),
-            "contrail-query-engine": module("--contrail-qe", "-qe", "contrail-query-engine", "8091"),
-            "contrail-analytics-nodemgr": module("--nodemgr-analytics", "-nodemgr-anal", "contrail-analytics-nodemgr", "8104"),
-            "contrail-config-nodemgr": module("--nodemgr-config", "-nodemgr-cfg", "contrail-config-nodemgr", "8100"),
-            "contrail-control-nodemgr": module("--nodemgr-control", "-nodemgr-cnt", "contrail-control-nodemgr", "8101"),
-            "contrail-database-nodemgr": module("--nodemgr-databsae", "-nodemgr-db", "contrail-database-nodemgr", "8103"),
-            "contrail-vrouter-nodemgr": module("--nodemgr-vrouter", "-nodemgr-vr", "contrail-vrouter-nodemgr", "8102")
-        }
         self.__all_args = self.parse_all_args()
         self.__debug = True if self.__all_args.debug else False
         self.__version = True if self.__all_args.version else False
 
-    def parse_all_args(self, args=None):
+    @staticmethod
+    def parse_all_args(args=None):
         conf_parser = argparse.ArgumentParser()
         conf_parser.add_argument("-c", "--config",
                                  help="path to YAML configuration file",
                                  metavar="FILE")                                       
-        for arg_opts in self.args.values():
+        for arg_opts in margs.values():
             conf_parser.add_argument(arg_opts.option, arg_opts.name, \
                 action='append', metavar="<ip-addr>", help="IP address or hostname to fetch introspect from \
                     {} module".format(arg_opts.help))
@@ -56,7 +38,7 @@ class ConfigParser():
         else:
             for key, val in self.__all_args.__dict__.items():
                  if val is not None and isinstance(val, list):
-                     all_formatted_args.append(module_args(key, val, self.args.get(key).port))
+                     all_formatted_args.append(module_args(key, val, margs.get(key).port))
         yield from all_formatted_args
     
     @classmethod
