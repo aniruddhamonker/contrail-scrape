80b223ef375048d7f39d8a83858d238c38299911
aniruddhamonker <aniruddh.amonker@live.com>
2020-08-05 18:34:00 -0700

    separating introspects and analytics apis in main

--- src/ContrailIntrospectScrape/__main__.py
+++ src/ContrailIntrospectScrape/__main__.py
@@ -1,23 +1,41 @@
 from config_parser import ConfigParser
 from introspects import IntrospectClass
+from analytics_uves import AnalyticsApiClass
+from base import BaseClass
 import sys
 
 version = "0.1.0"
 
+def get_version():
+    print("CONTRAIL-INTROSPECT-SCRAPE\nVERSION: {}".format(version))
+    sys.exit()
+
+def fetch_all_introspects(introspect_args, debug):
+    if introspect_args:
+        introspect = IntrospectClass(introspect_args, 50, debug)
+        return introspect.fetch_all_introspects()
+
+def fetch_analytics_api(api_args, debug):
+    if api_args:
+        analytics_api = AnalyticsApiClass(debug)
+        return analytics_api.fetch_all_analytics_apis(api_args)
+
 def main():
-    all_nodes = ConfigParser()
-    # import pdb; pdb.set_trace()
-    if all_nodes.version:
-        print("CONTRAIL-INTROSPECT-SCRAPE\nVERSION: {}".format(version))
-        sys.exit()
-    introspect = IntrospectClass(all_nodes(), 50, debug=all_nodes.debug)
+    config_parser = ConfigParser()
+    all_args = config_parser()
+    base = BaseClass()
+    if config_parser.version:
+        get_version()
     try:
-        introspect.fetch_all_introspects()
-        introspect.archive_all_files()
+        introspect_args = list(filter(lambda arg: arg.get('type') == "introspect", all_args))
+        fetch_all_introspects(introspect_args, config_parser.debug)
+        analytics_api_args = list(filter(lambda arg: arg.get('type') == "analytics-api", all_args))
+        fetch_analytics_api(analytics_api_args, config_parser.debug)
+        base.archive_all_files()
     except KeyboardInterrupt:
         print("Interrupted\n")
         print("Performing Cleanup...")
-        introspect.delete_tmp_files()
+        base.delete_tmp_files()
         sys.exit(0)
 
 if __name__ == "__main__":
