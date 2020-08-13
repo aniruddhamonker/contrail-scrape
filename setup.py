2fc65ea2bcef8e3b9de11cb78b1b980a1ab2fb8b
aniruddhamonker <aniruddh.amonker@live.com>
2020-08-12 18:06:57 -0700

    creating setup.py

--- setup.py
+++ setup.py
@@ -7,28 +7,28 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-# This call to setup() does all the work
 setup(
     name="contrail-scrape",
     version="0.1.0",
     description="crawler for all the APIs of Juniper Contrail nodes",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/realpython/reader",
-    author="Real Python",
-    author_email="office@realpython.com",
+    url="https://github.com/aniruddhamonker/contrail-scrape",
+    author="Aniruddh Amonker",
+    author_email="aamonker@juniper.net",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
     ],
-    packages=["reader"],
+    packages=["ContrailScrape"],
     include_package_data=True,
-    install_requires=["feedparser", "html2text"],
+    install_requires=["beautifulsoup4", "tqdm", "lxml", "PrettyTable", "html5lib" ],
     entry_points={
         "console_scripts": [
-            "realpython=reader.__main__:main",
+            "contrail-scrape=ContrailScrape.__main__:main",
+            "contrail-scrape-ist=ContrailScrape.contrail-scrape-ist:main",
         ]
     },
 )
